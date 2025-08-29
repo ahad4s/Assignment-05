fba8be6ee2c69dff513a2ee676f65a85259ab5b8
Your Name <you@example.com>
2025-08-29 17:12:50 +0600

    call history & clear history aded

--- srcipt.js
+++ srcipt.js
@@ -27,10 +27,33 @@ let copyCount = 0;
       copyCountElement.textContent = copyCount;
     });
   });
-/*calling function*/
+/*Call History & Clear History*/
+const callHistory = document.getElementById("callHistory");
+const clearBtn = document.getElementById("clearHistory");
+
 document.querySelectorAll(".call-btn").forEach(btn => {
-  btn.addEventListener("click", (e) => {
+  btn.addEventListener("click", () => {
     const serviceName = btn.dataset.name;
-    alert("Calling " + serviceName);
+    const serviceNumber = btn.dataset.number;
+
+    alert("Calling " + serviceName + serviceNumber);
+
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+
+    const entry = document.createElement("div");
+    entry.className = "bg-neutral-50 p-5 rounded shadow flex justify-between border-b pb-2";
+    entry.innerHTML = `
+      <div>
+        <p class="text-sm font-semibold">${serviceName}</p>
+        <p class="text-xs text-gray-500">${serviceNumber}</p>
+      </div>
+      <span class="text-xs text-gray-400">${time}</span>
+    `;
+    callHistory.prepend(entry);
   });
+});
+
+clearBtn.addEventListener("click", () => {
+  callHistory.innerHTML = "";
 });
